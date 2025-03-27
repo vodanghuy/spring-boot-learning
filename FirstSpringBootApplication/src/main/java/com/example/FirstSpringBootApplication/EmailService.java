a586903fdcc91982c20bf3396de65d083b8aae44
Nhom 18 <iamdaghuy@gmail.com>
2025-03-27 21:20:41 +0700

    IoC (Inversion of Control)

--- FirstSpringBootApplication/src/main/java/com/example/FirstSpringBootApplication/EmailService.java
+++ FirstSpringBootApplication/src/main/java/com/example/FirstSpringBootApplication/EmailService.java
@@ -1,5 +1,8 @@
 package com.example.FirstSpringBootApplication;
 
+import org.springframework.stereotype.Component;
+
+@Component // Spring Boot sẽ quản lý Class này, khi dùng không cần khởi tạo, chỉ cần lấy từ Spring Container
 public class EmailService implements MessageService {
 
     @Override
--- FirstSpringBootApplication/src/main/java/com/example/FirstSpringBootApplication/FirstSpringBootApplication.java
+++ FirstSpringBootApplication/src/main/java/com/example/FirstSpringBootApplication/FirstSpringBootApplication.java
@@ -2,16 +2,17 @@ package com.example.FirstSpringBootApplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class FirstSpringBootApplication {
 
 	public static void main(String[] args) {
-
-//		SpringApplication.run(FirstSpringBootApplication.class, args);
-		MessageService messageService = new SMSService();
-		Client client = new Client(messageService);
-		client.SendMessage("Hello World!");
+		// ApplicationContext
+		ApplicationContext context = SpringApplication.run(FirstSpringBootApplication.class, args);
+		// Sử dụng Email Service mà không cần khởi tạo
+		EmailService emailService = context.getBean(EmailService.class);
+		emailService.SendMessage("Hello World"); // Email Message: Hello World
 	}
 
 }
