0098b99ac35161621d075bf886e814957c9da41a
Nhom 18 <iamdaghuy@gmail.com>
2025-03-27 21:31:00 +0700

    DI in IoC

--- FirstSpringBootApplication/src/main/java/com/example/FirstSpringBootApplication/Client.java
+++ FirstSpringBootApplication/src/main/java/com/example/FirstSpringBootApplication/Client.java
@@ -1,12 +1,17 @@
 package com.example.FirstSpringBootApplication;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Client {
+    /*
+    Thay vì sử dụng Constructor Injection, Annotation này sẽ
+    giúp lớp Client tự động tiêm MessageService vào chính nó
+     */
+    @Autowired
     MessageService messageService;
 
-    public Client (MessageService messageService)
-    {
-        this.messageService = messageService;
-    }
     public void SendMessage(String message)
     {
         messageService.SendMessage(message);
